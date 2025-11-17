02909705e77647c985661303d0cd83f293883bac
EgorBeschetnov <0e.besch0@gmail.com>
2025-11-17 20:10:52 +0700

    создал структуру проекта и модель Valera(valera.cs)

---
+++ lab1/valera_api/models/valera.cs
@@ -0,0 +1,102 @@
+namespace valera_api.models
+{
+	public class Valera
+	{
+		public int Health { get; private set; }
+		public int Mana { get; private set; }
+		public int Cheerfulness { get; private set; }
+		public int Fatigue { get; private set; }
+		public int Money { get; private set; }
+
+		public Valera(int Health = 100, int Mana = 0, int Cheerfulness = 0, int Fatigue = 0, int Money = 100)
+		{
+			Health = ValidateRange(health, 0, 100);
+			Mana = ValidateRange(mana, 0, 100);
+			Cheerfulness = ValidateRange(cheerfulness, -10, 10);
+			Fatigue - ValidateRange(fatigue, 0, 100);
+			Money = money;
+		}
+
+		public bool GoToWork()
+		{
+			if (Mana >= 50 || Fatigue >= 10)
+				return false;
+
+			Cheerfulness = ValidateRange(Cheerfulness + 1, -10, 10);
+			Mana = ValidateRange(Mana -30, 0, 100);
+			Money += 100;
+			Fatigue = ValidateRange(Fatigue +10, 0, 100);
+			return true;
+		}
+
+		public void ContemplateNature()
+		{
+			Cheerfulness = ValidateRange(Cheerfulness + 1, -10, 10);
+			Mana = ValidateRange(Mana - 10, 0, 100);
+			Fatigue = ValidateRange( Fatigue + 10, 0, 100);
+		}
+
+		public void DrinkWinAndWatchTV()
+		{
+			Cheerfulness = ValidateRange(Cheerfulness -1, -10, 10);
+			Mana = ValidateRange(Mana + 30, 0, 100);
+			Fatigue = ValidateRange(Fatigue + 10, 0, 100);
+			Health = ValidateRange(Health - 5, 0, 100);
+			Money -= 20;
+		}
+
+		public void GoToBar()
+		{
+			Cheerfulness = ValidateRange(Cheerfulness + 1, -10, 10);
+			Mana = ValidateRange(Mana + 60, 0, 100);
+			Fatigue = ValidateRange(Fatigue + 40, 0, 100);
+			Health = ValidateRange(Health -10, 0, 100);
+			Money -= 100;
+		}
+
+		public void DronkWithMarginals()
+		{
+			Cheerfulness = ValidateRange(Cheerfulness + 5, -10, 10);
+			Health = ValidateRange(Health - 80, 0, 100);
+			Mana = ValidateRange(Mana + 90, 0, 100);
+			Fatigue = ValidateRange(Fatigue + 80, 0, 100);
+			Money -= 150;
+		}
+
+		public void SingInSubway()
+		{
+			Cheerfulness = ValidateRange(Cheerfulness + 1, -10, 10);
+			Mana = ValidateRange(Mana + 10, 0, 100);
+			Fatigue = ValidateRange(Fatigue + 20, 0, 100);
+
+			Money += 10;
+			if (Mana > 40 && Mana < 70)
+			{
+				Money += 50;
+			}
+		}
+
+		public void Sleep()
+		{
+			if (Mana < 30)
+			{
+				Health = ValidateRange(Health + 90, 0, 100);
+			}
+
+			if (Mana > 70)
+			{
+				Cheerfulness = ValidateRange(Cheerfulness - 3, -10, 10);
+			}
+
+			Mana = ValidateRange(Mana - 50, 0, 100);
+			Fatigue = ValidateRange(Fatigue - 70, 0, 100);
+		
+		}
+
+		private int ValidateRange(int value, int min, int max)
+		{
+			return Math.Min(Math.Max(value, min), max);
+		}
+		
+	}
+}
